1549c99af7363ce17da9b1e29d4b59229327df1b
Pace <pacex3123@gmail.com>
2021-07-22 20:24:32 +0200

    Recoded a little

--- Information Plus/src/main/java/me/georgeawp/informationplus/Informationplus.java
+++ Information Plus/src/main/java/me/georgeawp/informationplus/Informationplus.java
@@ -1,105 +1,35 @@
 package me.georgeawp.informationplus;
 
-import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
+import me.georgeawp.informationplus.commands.InformationCommands;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Informationplus extends JavaPlugin {
 
+    public static Informationplus instance = null;
+    public FileConfiguration config;
+
+    public static Informationplus getInstance() {
+        return instance;
+    }
+
+    public void reloadConfiguration() {
+        this.reloadConfig();
+        config = this.getConfig();
+    }
+
+
     @Override
     public void onEnable() {
-        this.saveDefaultConfig();
-        // Plugin startup logic
-
+        Informationplus.instance = this;
+        config = this.getConfig();
+        config.options().copyDefaults(true);
+        this.saveConfig();
+        getCommand("server").setExecutor(new InformationCommands());
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
     }
-
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-
-        // /serverinformation
-        if (label.equalsIgnoreCase("server")) {
-            if (!sender.hasPermission("InformationPlus.main")) {
-                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("main.error")));
-                return true;
-            }
-            if (args.length == 0) {
-                // /server
-                if (sender.hasPermission("InformationPlus.main")) {
-                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&' , this.getConfig().getString("commands.border")));
-                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&' , this.getConfig().getString("commands.header")));
-                    for(String commands : this.getConfig().getStringList("commands.lines")) {
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', commands));
-                    }
-                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&' , this.getConfig().getString("commands.border")));
-                    return true;
-                }
-                return true;
-            }
-            if (args.length > 0) {
-                // /server reload
-                if (args[0].equalsIgnoreCase("reload")) {
-                    if (sender.hasPermission("InformationPlus.reload")) {
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("reload.message")));
-                        this.reloadConfig();
-                        return true;
-                    } else {
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("reload.error")));
-                        return true;
-                    }
-                }
-
-                if (args[0].equalsIgnoreCase("links")) {
-                    if (sender.hasPermission("InformationPlus.main")) {
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&' , this.getConfig().getString("links.border")));
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&' , this.getConfig().getString("links.header")));
-                        for(String links : this.getConfig().getStringList("links.lines")) {
-                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', links));
-                        }
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&' , this.getConfig().getString("links.border")));
-                        return true;
-                    } else {
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("main.error")));
-                    }
-                }
-
-                if (args[0].equalsIgnoreCase("rules")) {
-                    if (sender.hasPermission("InformationPlus.main")) {
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("rules.border")));
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("rules.header")));
-                        for (String rules : this.getConfig().getStringList("rules.lines")) {
-                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', rules));
-                        }
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("rules.border")));
-                        return true;
-                    } else {
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("main.error")));
-                    }
-                }
-                if(args[0].equalsIgnoreCase("help")) {
-                    if(sender.hasPermission("InformationPlus.main")) {
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&' , this.getConfig().getString("help.border")));
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&' , this.getConfig().getString("help.header")));
-                        for(String help : this.getConfig().getStringList("help.lines")) {
-                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', help));
-                        }
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&' , this.getConfig().getString("help.border")));
-                        return true;
-                    }
-                } else {
-                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&' , this.getConfig().getString("main.unknown")));
-                    return true;
-                }
-            }
-
-        }
-        return false;
-
-    }
-
-
 }
---
+++ Information Plus/src/main/java/me/georgeawp/informationplus/commands/InformationCommands.java
@@ -0,0 +1,87 @@
+package me.georgeawp.informationplus.commands;
+
+import me.georgeawp.informationplus.Informationplus;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class InformationCommands implements CommandExecutor {
+
+    public boolean onCommand(final CommandSender sender, final Command command, final String label, final String[] args) {
+            if (label.equalsIgnoreCase("server")) {
+                if (!sender.hasPermission("InformationPlus.main")) {
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', Informationplus.getInstance().config.getString("main.error")));
+                    return true;
+                }
+                if (args.length == 0) {
+                    // /server
+                    if (sender.hasPermission("InformationPlus.main")) {
+                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', Informationplus.getInstance().config.getString("commands.border")));
+                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', Informationplus.getInstance().config.getString("commands.header")));
+                        for (String commands : Informationplus.getInstance().config.getStringList("commands.lines")) {
+                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', commands));
+                        }
+                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', Informationplus.getInstance().config.getString("commands.border")));
+                        return true;
+                    }
+                    return true;
+                }
+                // /server reload
+                if (args[0].equalsIgnoreCase("reload")) {
+                    if (sender.hasPermission("InformationPlus.reload")) {
+                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', Informationplus.getInstance().config.getString("reload.message")));
+                        Informationplus.getInstance().reloadConfiguration();
+                        return true;
+                    } else {
+                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', Informationplus.getInstance().config.getString("reload.error")));
+                        return true;
+                    }
+                }
+
+                if (args[0].equalsIgnoreCase("links")) {
+                    if (sender.hasPermission("InformationPlus.main")) {
+                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', Informationplus.getInstance().config.getString("links.border")));
+                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', Informationplus.getInstance().config.getString("links.header")));
+                        for (String links : Informationplus.getInstance().config.getStringList("links.lines")) {
+                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', links));
+                        }
+                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', Informationplus.getInstance().config.getString("links.border")));
+                        return true;
+                    } else {
+                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', Informationplus.getInstance().config.getString("main.error")));
+                    }
+                }
+
+                if (args[0].equalsIgnoreCase("rules")) {
+                    if (sender.hasPermission("InformationPlus.main")) {
+                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', Informationplus.getInstance().config.getString("rules.border")));
+                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', Informationplus.getInstance().config.getString("rules.header")));
+                        for (String rules : Informationplus.getInstance().config.getStringList("rules.lines")) {
+                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', rules));
+                        }
+                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', Informationplus.getInstance().config.getString("rules.border")));
+                        return true;
+                    } else {
+                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', Informationplus.getInstance().config.getString("main.error")));
+                    }
+                }
+                if (args[0].equalsIgnoreCase("help")) {
+                    if (sender.hasPermission("InformationPlus.main")) {
+                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', Informationplus.getInstance().config.getString("help.border")));
+                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', Informationplus.getInstance().config.getString("help.header")));
+                        for (String help : Informationplus.getInstance().config.getStringList("help.lines")) {
+                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', help));
+                        }
+                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', Informationplus.getInstance().config.getString("help.border")));
+                        return true;
+                    }
+                } else {
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', Informationplus.getInstance().config.getString("main.unknown")));
+                    return true;
+                }
+                return false;
+            }
+            return false;
+        }
+    }
